34facabd1af4ec4bb03d411a622791a3536f4410
MzSuwawa <yudharamadhan838@gmail.com>
2024-06-03 15:12:08 +0700

    Menambahkan int main untuk awal program lalu menambahkan try dan memanggill array di dalamnya

--- Exception Standard/Exception Standard.cpp
+++ Exception Standard/Exception Standard.cpp
@@ -4,3 +4,13 @@
 #include <array>
 //Untuk objek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+	cout << "Awal Program" << endl; //Penanda 1:...
+	try {
+		array<int, 3> data = { 8, 4, 2 };
+		//Pesan array interger 3 elemen
+		cout << data.at(5) << endl;
+	}
+}
